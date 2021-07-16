29dacd0c2d7d5b596ae8393e74fcf90f41d6ad9b
egaviria <egaviria@MacBook-Pro-de-egaviria.local>
2021-07-16 11:33:57 -0500

    1

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { merge } from 'rxjs';
+import { filter, map, switchMap } from 'rxjs/operators';
+
+import { environment } from '@env/environment';
+import { Logger, UntilDestroy, untilDestroyed } from '@core';
+
+const log = new Logger('App');
+
+@UntilDestroy()
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit, OnDestroy {
+
+  constructor(private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title,
+  ) { }
+
+  ngOnInit() {
+    // Setup logger
+    if (environment.production) {
+      Logger.enableProductionMode();
+    }
+
+    log.debug('init');
+
+    const onNavigationEnd = this.router.events.pipe(filter(event => event instanceof NavigationEnd));
+  }
+
+  ngOnDestroy() {
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { CoreModule } from '@core';
+import { SharedModule } from '@shared';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot(),
+    NgbModule,
+    CoreModule,
+    SharedModule,
+    AppRoutingModule // must be imported as the last module as it contains the fallback route
+  ],
+  declarations: [AppComponent],
+  providers: [
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
